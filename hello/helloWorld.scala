54c30bc8a36c5f73b6f36227f38b2db51f053b84
AshidaSRS <ashida.shin@gmail.com>
2016-09-24 20:09:18 +0200

    Fix things

---
+++ hello/helloWorld.scala
@@ -0,0 +1,9 @@
+object Hola {
+  def main(args: Array[String]) = println("Hello World!")
+  def hola(parametros: Int) = {
+    print(parametros)
+  }
+  def queTal(nombre: String) = {
+    print("hola "+ nombre )
+  }
+}
